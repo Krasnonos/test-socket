3e2de60a5032fd22ce87955fdc0d4d859e40d2e5
Krasnonos <andreykrasnonos@icloud.com>
2022-08-24 18:11:59 +0300

    test

---
+++ src/components/App.jsx
@@ -0,0 +1,5 @@
+import ChatPage from './chat';
+
+export const App = () => {
+  return <ChatPage />;
+};
---
+++ src/components/chat.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:3001');
+
+const ChatPage = () => {
+  const [message, setMessage] = useState('');
+  const [messageRecived, setMessageRecived] = useState([]);
+  const [messageSended, setMessageSended] = useState([]);
+
+  useEffect(() => {
+    socket.on('recive_message', data => {
+      setMessageRecived([...messageRecived, data.message]);
+    });
+  }, [messageRecived]);
+
+  const sendMessage = e => {
+    e.preventDefault();
+    socket.emit('send_message', { message: message });
+    console.log(message);
+    setMessageSended([...messageSended, message]);
+    setMessage('');
+  };
+
+  return (
+    <>
+      <form onSubmit={sendMessage}>
+        <input
+          type="text"
+          placeholder="message..."
+          onChange={e => {
+            setMessage(e.target.value);
+          }}
+        />
+        <button type="submit">Send message</button>
+      </form>
+      {messageRecived.map(msg => (
+        <p key={msg} className="recived">
+          {msg}
+        </p>
+      ))}
+      {messageSended.map(msg => (
+        <p key={msg} className="recived">
+          {msg}
+        </p>
+      ))}
+    </>
+  );
+};
+
+export default ChatPage;
